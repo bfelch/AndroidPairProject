865363af7cad9c3c656432cb8b97056241da7f60
bfelch <kagrenac@live.com>
2015-03-04 17:02:01 -0600

    stored values
    values saved between updates
    
    min time between updates is 30 minutes and i don't want to set up a
    timer to trigger it more often
    
    i need to change the amount the values are lowered by

--- app/src/main/java/com/brandonfelch/widgeet/MainWidget.java
+++ app/src/main/java/com/brandonfelch/widgeet/MainWidget.java
@@ -3,6 +3,9 @@ package com.brandonfelch.widgeet;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 
@@ -10,6 +13,15 @@ import android.widget.RemoteViews;
  * Implementation of App Widget functionality.
  */
 public class MainWidget extends AppWidgetProvider {
+
+    public static final String PREFS_NAME = "PixelPetPrefs";
+
+    static float feedValue, playValue, washValue;
+    static float feedTimesPerDay = 2f;
+    static float playTimesPerDay = 4f;
+    static float washTimesPerDay = 1f;
+
+    static boolean didInit;
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -24,6 +36,7 @@ public class MainWidget extends AppWidgetProvider {
     @Override
     public void onEnabled(Context context) {
         // Enter relevant functionality for when the first widget is created
+        didInit = false;
     }
 
     @Override
@@ -34,10 +47,47 @@ public class MainWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.main_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        feedValue = prefs.getFloat("feed", 0.0f);
+        playValue = prefs.getFloat("play", 0.0f);
+        washValue = prefs.getFloat("wash", 0.0f);
+        didInit = prefs.getBoolean("init", false);
+
+        SharedPreferences.Editor edit = prefs.edit();
+
+        // Update values
+        if (didInit) {
+            feedValue -= (1 / feedTimesPerDay) - .05f;
+            playValue -= (1 / playTimesPerDay) - .05f;
+            washValue -= (1 / washTimesPerDay) - .05f;
+
+            Log.d("updateValues", "did Update");
+        } else {
+            feedValue = 1f;
+            playValue = 1f;
+            washValue = 1f;
+
+            didInit = true;
+
+            edit.putBoolean("init", didInit);
+
+            Log.d("updateValues", "did Init");
+        }
+
+        edit.putFloat("feed", feedValue);
+        edit.putFloat("play", playValue);
+        edit.putFloat("wash", washValue);
+
+        edit.commit();
+
+        Log.d("updateValues", "in Update");
+
+        views.setTextViewText(R.id.feedVal, String.format("%.2f", feedValue * 100) + "%");
+        views.setTextViewText(R.id.playVal, String.format("%.2f", playValue * 100) + "%");
+        views.setTextViewText(R.id.washVal, String.format("%.2f", washValue * 100) + "%");
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
